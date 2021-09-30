2b969615b6c5028d3d1d9d747e0f1b6abb8c3b2b
Daiane <daianemarcon@gmail.com>
2021-09-30 20:54:08 -0300

    ssl postgres heroku

--- DB/db.js
+++ DB/db.js
@@ -1,11 +1,20 @@
-//require('dotenv').config();
+require('dotenv').config();
 
-const { Sequelize } = require("sequelize");
+const {
+  Sequelize
+} = require("sequelize");
 
 //postgres://usuario:senha@servidor:porta/banco
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
-  { dialect: "postgres" }
+  process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  }
+  }
 );
 
 (async () => {
